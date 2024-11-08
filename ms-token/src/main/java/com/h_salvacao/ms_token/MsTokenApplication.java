eb3efcd68fdcdc016c18ecb4dba46a20f8b7d3fa
GabrielF-S <gabrielboni2008@hotmail.com>
2024-11-08 13:28:51 -0300

    Criação do microsserviço de Token

---
+++ ms-token/src/main/java/com/h_salvacao/ms_token/MsTokenApplication.java
@@ -0,0 +1,13 @@
+package com.h_salvacao.ms_token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTokenApplication.class, args);
+	}
+
+}
---
+++ ms-token/src/test/java/com/h_salvacao/ms_token/MsTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h_salvacao.ms_token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
