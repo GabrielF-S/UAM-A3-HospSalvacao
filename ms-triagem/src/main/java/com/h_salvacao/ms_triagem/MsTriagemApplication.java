358ec51b4b6540d9b132c5a45f23267217a1a212
GabrielF-S <gabrielboni2008@hotmail.com>
2024-11-17 18:58:35 -0300

    Criado microsserviço para fila de Triagem

---
+++ ms-triagem/src/main/java/com/h_salvacao/ms_triagem/MsTriagemApplication.java
@@ -0,0 +1,13 @@
+package com.h_salvacao.ms_triagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTriagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTriagemApplication.class, args);
+	}
+
+}
---
+++ ms-triagem/src/test/java/com/h_salvacao/ms_triagem/MsTriagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h_salvacao.ms_triagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTriagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
