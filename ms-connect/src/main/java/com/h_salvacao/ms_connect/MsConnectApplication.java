33fe5f4bd85eedbacd0fadba45a26e67156981a1
GabrielF-S <gabrielboni2008@hotmail.com>
2024-11-19 16:39:41 -0300

    Criado ms responsavel pela conexão dom a database

---
+++ ms-connect/src/main/java/com/h_salvacao/ms_connect/MsConnectApplication.java
@@ -0,0 +1,13 @@
+package com.h_salvacao.ms_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsConnectApplication.class, args);
+	}
+
+}
---
+++ ms-connect/src/test/java/com/h_salvacao/ms_connect/MsConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h_salvacao.ms_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
