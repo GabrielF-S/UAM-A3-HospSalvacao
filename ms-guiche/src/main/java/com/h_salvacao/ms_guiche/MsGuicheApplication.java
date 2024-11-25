c930947f804233819e93778616b675b818a6c12a
GabrielF-S <gabrielboni2008@hotmail.com>
2024-11-25 13:09:49 -0300

    Criado microsserviço responsavel pelo chamado no Guiche

---
+++ ms-guiche/src/main/java/com/h_salvacao/ms_guiche/MsGuicheApplication.java
@@ -0,0 +1,13 @@
+package com.h_salvacao.ms_guiche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGuicheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGuicheApplication.class, args);
+	}
+
+}
