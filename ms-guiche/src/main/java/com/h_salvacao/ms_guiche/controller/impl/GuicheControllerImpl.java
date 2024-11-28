98a2976f1723e0b9d0ce053c2ec3ad3dac6c4f57
GabrielF-S <gabrielboni2008@hotmail.com>
2024-11-28 02:28:32 -0300

    Liberado o CORS para aplicação

--- ms-guiche/src/main/java/com/h_salvacao/ms_guiche/controller/impl/GuicheControllerImpl.java
+++ ms-guiche/src/main/java/com/h_salvacao/ms_guiche/controller/impl/GuicheControllerImpl.java
@@ -5,11 +5,14 @@ import com.h_salvacao.ms_guiche.model.Token;
 import com.h_salvacao.ms_guiche.service.GuicheService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "guiche")
+@CrossOrigin(value = "http://localhost:4200/", methods = {RequestMethod.GET, RequestMethod.PUT, RequestMethod.POST })
 public class GuicheControllerImpl implements GuicheController {
 
     @Autowired
