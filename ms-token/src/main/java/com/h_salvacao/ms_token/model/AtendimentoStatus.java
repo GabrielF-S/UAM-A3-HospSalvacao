a269f446f60665fa8bbf2a44cf350dc611f66ad7
GabrielF-S <gabrielboni2008@hotmail.com>
2024-12-01 22:34:01 -0300

    Renomeado Enumerate de AtendimentoStatus para melhor compreenção

--- ms-token/src/main/java/com/h_salvacao/ms_token/model/AtendimentoStatus.java
+++ ms-token/src/main/java/com/h_salvacao/ms_token/model/AtendimentoStatus.java
@@ -1,13 +1,13 @@
 package com.h_salvacao.ms_token.model;
 
 public enum AtendimentoStatus {
-    AGUARD_TRIAGEM,
-    AGUARD_GUICHE,
-    AGUARD_DOUTOR,
-    AGUARD_MEDICACAO,
-    AGUARD_RAIOX,
-    AGUARD_MED_RAIOX,
-    AGUARD_RAIOX_MED
+    TRIAGEM,
+    GUICHE,
+    DOUTOR,
+    MEDICACAO,
+    RAIOX,
+    MED_RAIOX,
+    RAIOX_MED
 
 
 }
