fad9b52913ebbf2631d84e3a583903f779cf1d49
GabrielF-S <gabrielboni2008@hotmail.com>
2024-12-09 20:54:29 -0300

    Criado endpoint para consulta de ficha

--- ms-connect/src/main/java/com/h_salvacao/ms_connect/controller/FichaController.java
+++ ms-connect/src/main/java/com/h_salvacao/ms_connect/controller/FichaController.java
@@ -3,10 +3,7 @@ package com.h_salvacao.ms_connect.controller;
 import com.h_salvacao.ms_connect.entity.Ficha;
 import com.h_salvacao.ms_connect.entity.Token;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 public interface FichaController {
 
@@ -15,7 +12,7 @@ public interface FichaController {
 
 
     @GetMapping(value = "get/{tokenId}")
-    ResponseEntity<Ficha> getFicha(@RequestParam Long tokenId);
+    ResponseEntity<Ficha> getFicha(@PathVariable("tokenId") Long tokenId);
 
     @PostMapping(value = "update")
     ResponseEntity<Ficha> atualizarFicha(@RequestBody Ficha ficha);
