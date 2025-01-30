aa0b157861dae4e4f7f1a8e3ee06b7251f60a072
GabrielF-S <gabrielboni2008@hotmail.com>
2025-01-30 17:23:23 -0300

    Organizado Imports

--- ms-medicacao/src/main/java/com/h_salvacao/ms_medicacao/services/MedicacaoProducerSender.java
+++ ms-medicacao/src/main/java/com/h_salvacao/ms_medicacao/services/MedicacaoProducerSender.java
@@ -2,8 +2,6 @@ package com.h_salvacao.ms_medicacao.services;
 
 import com.h_salvacao.ms_medicacao.model.Encaminhamento;
 import com.h_salvacao.ms_medicacao.model.Token;
-import com.h_salvacao.ms_medico.model.Encaminhamento;
-import com.h_salvacao.ms_medico.model.Token;
 
 public interface MedicacaoProducerSender {
 
