7f61281fbe1f03b9d87cfaf9458506e4226addd0
GabrielF-S <gabrielboni2008@hotmail.com>
2025-02-10 18:59:48 -0300

    :heavy_plus_sign: build: Adiconado dependencia do OpenFeign

--- ms-atendimento/src/main/java/com/h_salvacao/ms_atendimento/MsAtendimentoApplication.java
+++ ms-atendimento/src/main/java/com/h_salvacao/ms_atendimento/MsAtendimentoApplication.java
@@ -2,8 +2,9 @@ package com.h_salvacao.ms_atendimento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.kafka.annotation.EnableKafka;
-
+@EnableFeignClients
 @EnableKafka
 @SpringBootApplication
 public class MsAtendimentoApplication {
